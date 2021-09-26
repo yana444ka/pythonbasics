c06315ff1815d5688a10f5a702acc52f49800b6d
yana444ka <90350016+yana444ka@users.noreply.github.com>
2021-09-27 02:41:40 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -5,21 +5,23 @@ from functools import reduce
 from sys import argv
 
 script_name, wage, labour, bonus = argv
-print(script_name)
+print('Name of script:', script_name)
 print('Wage: ', wage)
 print('Hours worked: ', labour)
 print('Bonus: ', bonus)
 salary = int(wage) * int(labour) + int(bonus)
-print(salary)
+print('Salary:', salary)
 
 # 2 задание
-alist = [r.randint(-300,300) for i in range(r.randint(0,20))]
+# если список не задан
+# alist = [r.randint(-300,300) for i in range(r.randint(0,20))]
+alist = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 new_alist = [el for i, el in enumerate(alist) if alist[i] > alist[i-1]]
 print('Задание №2', f'Исходный список: {alist}', f'Новый список: {new_alist}', sep='\n')
 #решение не предусматривает вариант, когда в списке 1 элемент
 
 # 3 задание
-print('Задание №3', f'Итоговый список: {[i for i in range(20,241) if i % 20 == 0 or i % 21==0]}')
+print('Задание №3', f'Итоговый список: {[i for i in range(20,241) if i % 20 == 0 or i % 21 == 0]}')
 
 # 4 задание
 list1 = [r.randint(-300,300) for i in range(r.randint(0,20))]
@@ -33,6 +35,35 @@ def prod(prev_el, el):
 print('Задание №5', reduce(prod, my_list), sep='\n')
 
 # 6 задание
+from itertools import count, cycle
+print('Задание №6')
+x = int(input('Введите число: '))
+def gen_a(x):
+    for i in count(x):
+        if(i == x + 20):
+            break
+        print(i)
+gen_a(x)
+
+list = input('Введите список: ').split(' ')
+k = int(input('Сколько раз повторить список? '))
+
+def gen_b(list):
+    i = 0
+    for el in cycle(list):
+        if el == list[0]:
+            i += 1
+        if i <= k:
+            print(el)
+gen_b(list)
 
 
 # 7 задание
+n = int(input('Введите число: '))
+def fact(n):
+    a = 1
+    for i in count(1):
+        if i <= n:
+            a *= i
+            print(a)
+fact(n)
